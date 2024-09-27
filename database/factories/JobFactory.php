8568ef2bc19e9cef725284ded8ab6b002f3c5899
nicollas.lopes <nicollaslopesn@gmail.com>
2024-09-26 21:05:19 -0300

    feat: creating Job factory/seeder

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [ 
+            'id_company' => Company::all()->random()->id, 
+            'title' => fake()->jobTitle(), 
+            'salary' => fake()->randomFloat(2, 1000, 10000), 
+            'created_at' => now(), 
+            'updated_at' => now()
+        ];
+    }
+}
