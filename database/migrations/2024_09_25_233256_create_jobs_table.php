1c3fb2049c9ed3ce08364c4a9ba5787b395d7ad0
nicollas.lopes <nicollaslopesn@gmail.com>
2024-10-05 17:11:59 -0300

    refator: changing job and users migration

--- database/migrations/2024_09_25_233256_create_jobs_table.php
+++ database/migrations/2024_09_25_233256_create_jobs_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('id_company')->references('id')->on('companies');
+            $table->foreignId('id_recruiter')->references('id')->on('users');
             $table->string('title');
             $table->string('city');
             $table->string('state');
