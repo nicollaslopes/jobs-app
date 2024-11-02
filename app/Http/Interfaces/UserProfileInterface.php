de9057ff62efce14ca96cd81b09a1b1809a9f386
nicollaslopes <nicollaslopesn@gmail.com>
2024-11-02 20:09:23 -0300

    feat: removing and add user technologies

--- app/Http/Interfaces/UserProfileInterface.php
+++ app/Http/Interfaces/UserProfileInterface.php
@@ -5,4 +5,5 @@ namespace App\Http\Interfaces;
 interface UserProfileInterface
 {
     public static function add($request): bool;
+    public static function destroy($request): bool;
 }
--- app/Http/Repositories/UserProfileRepository.php
+++ app/Http/Repositories/UserProfileRepository.php
@@ -5,6 +5,7 @@ namespace App\Http\Repositories;
 use App\Http\Interfaces\UserProfileInterface;
 use App\Models\UserTechnology;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserProfileRepository implements UserProfileInterface
 {
@@ -20,6 +21,21 @@ class UserProfileRepository implements UserProfileInterface
                 ]);
             }
 
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+
+    public static function destroy($data): bool
+    {
+        $user = Auth::user();
+
+        try {
+            if ($user) {
+                DB::table('user_technologies')->where('id_user', $user->id)->delete();
+            }
+            
             return true;
         } catch (\Throwable $th) {
             return false;
