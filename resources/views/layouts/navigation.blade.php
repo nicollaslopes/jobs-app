36b5091e68241add0fa7570441fe7d8baefe5dc1
nicollaslopes <nicollaslopesn@gmail.com>
2024-11-04 20:55:35 -0300

    chore: changing logo image

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('images/job-logo.jpg') }}" style="max-width: 60px; max-height: 200px;" alt="Job Logo">
                     </a>
                 </div>
 
